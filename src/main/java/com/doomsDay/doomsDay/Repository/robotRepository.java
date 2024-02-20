e125901e680b189f240a8f878caf7e57808410d1
tonycarrel <133214532+tonycarrel@users.noreply.github.com>
2024-02-20 11:13:39 +0200

    Update robotRepository.java

--- src/main/java/com/doomsDay/doomsDay/Repository/robotRepository.java
+++ src/main/java/com/doomsDay/doomsDay/Repository/robotRepository.java
@@ -3,6 +3,6 @@ package com.doomsDay.doomsDay.Repository;
 import com.doomsDay.doomsDay.entity.Robot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface robotRepository extends JpaRepository<Robot, Long> {
+public interface robotRepository extends JpaRepository<Robot, String> {
 
 }
