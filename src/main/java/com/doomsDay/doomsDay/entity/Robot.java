6a58aaaeca82ff3dfbeeef4480f0487c19786473
tonycarrel <133214532+tonycarrel@users.noreply.github.com>
2024-02-20 11:02:40 +0200

    Update Robot.java

--- src/main/java/com/doomsDay/doomsDay/entity/Robot.java
+++ src/main/java/com/doomsDay/doomsDay/entity/Robot.java
@@ -2,18 +2,59 @@ package com.doomsDay.doomsDay.entity;
 
 public class Robot {
 
+    private String model;
+    private String serialNumber;
+    private String manufactureDate;
     private String category;
 
 
+
     // constructors
+
+    public Robot() {
+    }
+
     public Robot(String model, String serialNumber, String manufactureDate, String category) {
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.manufactureDate = manufactureDate;
         this.category = category;
+    }
+
+
+
+    //getters and setters
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(String manufactureDate) {
+        this.manufactureDate = manufactureDate;
     }
 
     public String getCategory() {
         return category;
     }
 
-    //getters and setters
-
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
